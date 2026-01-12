e3adaabd9bae9b15305a7f367380b3cb9ae624f0
DaanSpeelmansPXL <daan.speelmans@student.pxl.be>
2026-01-12 14:58:16 +0100

    commit 8 (finetuning2.0= extra foto's, extra ervaringen, totop-btn, toggledark tuning)

--- script.js
+++ script.js
@@ -74,3 +74,14 @@ document.addEventListener("DOMContentLoaded", () => {
         updateSlider();
     });
 });
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+    if(window.pageYOffset > 100) {
+        toTop.classList.add("active");
+    } else {
+        toTop.classList.remove("active");
+    }
+})
+
